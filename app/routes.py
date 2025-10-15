870cdd080eab027c479d8563896b52716327f147
ayushiraj02 <ayushimk14@gmail.com>
2025-10-15 18:59:11 +0530

    changes in run file

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+# routes.py
 from app import db
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,4 @@
+# utils.py
 import os
 import re
 from urllib.parse import urljoin, urlparse
